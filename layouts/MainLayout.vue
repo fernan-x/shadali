e6cbfb84726d36eef15d46752e220f1caef25802
Fernandes Alves Fabien <fabien@code42.fr>
2023-07-10 21:38:44 +0200

    feat: start creating navbar

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="mainLayout" class="w-full fixed z-50">
+    <div id="topMenu" class="w-ful bg-[#FAFAFA] border-b md:block hidden">
+      <ul
+        class="flex items-center justify-end text-xs text-[#333333] font-light px-2 h-10 bg-[#FAFAFA] max-w-[1200px]"
+      >
+        <li class="list-item">Sell on AliExpress</li>
+        <li class="list-item">Help</li>
+        <li class="list-item">Buyer Protection</li>
+        <li class="px-3 hover:text-[#FF4646] cursor-pointer">
+          <Icon name="ic:sharp-install-mobile" size="17" />
+          App
+        </li>
+        <li
+          @mouseenter="isAccountMenu = true"
+          @mouseleave="isAccountMenu = false"
+          class="relative flex items-center px-2.5 hover:text-[#FF4646] h-full cursor-pointer"
+          :class="
+            isAccountMenu
+              ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]'
+              : 'border-[#FAFAFA]'
+          "
+        >
+          <Icon name="ph:user-thin" size="17" />
+          Account
+          <Icon name="mdi:chevron-down" size="15" class="ml-5" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const isAccountMenu = ref(false);
+</script>
+
+<style>
+.list-item {
+  @apply border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer;
+}
+</style>
