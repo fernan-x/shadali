ed1ce1dd004b06066b805845eae56297635ae33d
Fernandes Alves Fabien <fabien@code42.fr>
2023-07-11 08:15:12 +0200

    feat: add logo + searchbar and cart

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="mainLayout" class="w-full fixed z-50">
-    <div id="topMenu" class="w-ful bg-[#FAFAFA] border-b md:block hidden">
+    <div id="topMenu" class="w-full bg-[#FAFAFA] border-b md:block hidden">
       <ul
         class="flex items-center justify-end text-xs text-[#333333] font-light px-2 h-10 bg-[#FAFAFA] max-w-[1200px]"
       >
@@ -24,14 +24,125 @@
           <Icon name="ph:user-thin" size="17" />
           Account
           <Icon name="mdi:chevron-down" size="15" class="ml-5" />
+
+          <div
+            id="accountMenu"
+            v-if="isAccountMenu"
+            class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
+          >
+            <div v-if="true">
+              <div class="text-semibold text-[15px] my-4 px-3">
+                Welcome to AliExpress!
+              </div>
+              <div class="flex items-center gap-1 px-3 mb-3">
+                <NuxtLink
+                  to="/auth"
+                  class="bg-[#FF4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2"
+                >
+                  Login / Register
+                </NuxtLink>
+              </div>
+            </div>
+            <div class="border-b"></div>
+            <ul class="bg-white">
+              <li
+                @click="navigateTo('/orders')"
+                class="text-[13px] py-2 px-4 hover:bg-gray-200"
+              >
+                My Orders
+              </li>
+              <li v-if="true" class="text-[13px] py-2 px-4 hover:bg-gray-200">
+                Sign out
+              </li>
+            </ul>
+          </div>
         </li>
       </ul>
+    </div>
+
+    <div id="mainHeader" class="flex items-center w-full bg-white">
+      <div
+        class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
+      >
+        <NuxtLink to="/" class="min-w-[170px]"
+          ><img width="170" src="/AliExpress-logo.png"
+        /></NuxtLink>
+
+        <div class="max-w-[700px] w-full md:block hidden">
+          <div class="relative">
+            <div
+              class="flex items-center border-2 border-[#FF4646] rounded-md w-full"
+            >
+              <input
+                class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none rounded-md"
+                placeholder="kitchen accessories"
+                type="text"
+                v-model="searchItem"
+              />
+              <Icon
+                v-if="isSearching"
+                name="eos-icons:loading"
+                size="25"
+                class="mr-2"
+              />
+              <button
+                class="flex items-center h-[100%] p-1.5% px-2 bg-[#FF4646]"
+              >
+                <Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
+              </button>
+            </div>
+
+            <div class="absolute bg-white max-w-[700px] h-auto w-full">
+              <div v-if="false" class="p-1">
+                <NuxtLink
+                  to="/item/1"
+                  class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+                >
+                  <div class="flex items-center">
+                    <img
+                      src="https://placehold.co/300x300"
+                      alt="sum"
+                      width="40"
+                      class="rounded-md"
+                    />
+                    <div class="truncate ml-2">TESTING</div>
+                  </div>
+                  <div class="truncate">$ 98.99</div>
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <NuxtLink to="/shoppingcart" class="flex items-center">
+          <button
+            class="relative md:block hidden"
+            @mouseenter="isCartHover = true"
+            @mouseleave="isCartHover = false"
+          >
+            <span
+              class="absolute flex items-center justify-center -right-[3px] top-0 bg-[#FF4646] h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
+              >0</span
+            >
+            <div class="min-w-[40px]">
+              <Icon
+                name="ph:shopping-cart-simple-light"
+                size="33"
+                :color="isCartHover ? '#FF4646' : ''"
+              />
+            </div>
+          </button>
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 const isAccountMenu = ref(false);
+const isCartHover = ref(false);
+const isSearching = ref(false);
+const searchItem = ref("");
 </script>
 
 <style>
